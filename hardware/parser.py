88f68cf662725765d897e4c99d17e8dd35ab815c
Vav <vkaranam_98@tamu.edu>
2021-04-13 13:11:27 -0500

    Update parser.py

--- hardware/parser.py
+++ hardware/parser.py
@@ -8,7 +8,19 @@ filename = "mmrh.csv"
 fields = []
 rows = []
 
-Clist =[]
+c_list  = []
+cs_list = []
+d_list  = []
+ds_list = []
+e_list  = []
+f_list  = []
+fs_list = []
+g_list  = []
+gs_list = []
+a_list  = []
+as_list = []
+b_list  = []
+c2_list  = []
 
 
 # reading csv file
@@ -30,45 +42,42 @@ for row in rows:
     Switch(row[2]){
 
     case 0:
-        .append(row[1])
+        c_list.append(row[1])
 
-    case 0:
-        .append(row[1])
+    case 1:
+        cs_list.append(row[1])
 
-    case 0:
-        .append(row[1])
+    case 2:
+        d_list.append(row[1])
 
-    case 0:
-        .append(row[1])
+    case 3:
+        ds_list.append(row[1])
 
-    case 0:
-        .append(row[1])
+    case 4:
+        e_list.append(row[1])
 
-    case 0:
-        .append(row[1])
+    case 5:
+        f_list.append(row[1])
 
-    case 0:
-        .append(row[1])
+    case 6:
+        fs_list.append(row[1])
 
-    case 0:
-        .append(row[1])
+    case 7:
+        g_list.append(row[1])
 
-    case 0:
-        .append(row[1])
+    case 8:
+        gs_list.append(row[1])
 
-    case 0:
-        .append(row[1])
+    case 9:
+        a_list.append(row[1])
 
-    case 0:
-        .append(row[1])
+    case 10:
+        as_list.append(row[1])
 
-    case 0:
-        .append(row[1])
+    case 11:
+        b_list.append(row[1])
 
-
-
+    case 12:
+        c2_list.append(row[1])
     }
-    print(row[1])
-
-
-    print('\n')
+    #print(row[1])
