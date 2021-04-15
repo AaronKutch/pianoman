e58d49d95c486bd5ace42beb81618778b907bc0a
aaronkutch1999 <aaronkutch1999@tamu.edu>
2021-04-15 12:38:52 -0500

    fix rest angles

--- hardware/zero_out.py
+++ hardware/zero_out.py
@@ -1,17 +1,11 @@
 import time
 
+restAngle = [85,75,70,80,65,75,90,80,75,70,60,75,60];
 
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
-for i in range(0,15):
-    kit.servo[i].angle = 90;
-  
-while(True):
-    kit.servo[6].angle = 180;
-    time.sleep(1)
-    kit.servo[6].angle = 160;
-    time.sleep(1)
-    
-    
+for i in range(0,len(restAngle)):
+    kit.servo[i].angle = restAngle[i];
+
 print("Servos Zeroed.");
