04cc379fa1f4a0a9566d7f5aa2fbe778d1d1c11c
aaronkutch1999 <aaronkutch1999@tamu.edu>
2021-04-15 14:36:10 -0500

    better camera

--- hardware/camera_test.py
+++ hardware/camera_test.py
@@ -1,25 +1,28 @@
 import cv2
-  
-  
-# define a video capture object
+import time
+
+# intended for a wansview 1080p camera
 vid = cv2.VideoCapture(0)
-  
+if not vid.isOpened():
+    raise IOError("Cannot open webcam")
+
+vid.set(cv2.CAP_PROP_FPS, 10) # the camera has maximum resolution at low framerate
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
 while(True):
-      
-    # Capture the video frame
-    # by frame
     ret, frame = vid.read()
   
     # Display the resulting frame
     cv2.imshow('frame', frame)
       
+    time.sleep(0.1)
+
     # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
+    # quitting button
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-  
-# After the loop release the cap object
+
+cv2.imwrite('omr/images/input.png', frame)
 vid.release()
-# Destroy all the windows
 cv2.destroyAllWindows()
